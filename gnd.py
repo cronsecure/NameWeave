dd6c2bb37af8d48d0265a4ab66306aa467280e6f
Cronsecure <112363156+Cronsecure@users.noreply.github.com>
2023-05-23 17:59:23 -0400

    Create gnd.py

---
+++ gnd.py
@@ -0,0 +1,34 @@
+import argparse
+
+def generate_username_combinations(names_lastnames):
+    combinations = []
+
+    for name, lastname in names_lastnames:
+        username1 = name.lower() + lastname.lower()
+        username2 = name.lower() + '.' + lastname.lower()
+        username3 = name[0].lower() + '.' + lastname.lower()
+        username4 = name[0].lower() + lastname.lower()
+        combinations.extend([username1, username2, username3, username4])
+
+    return combinations
+
+# Configurar el análisis de argumentos
+parser = argparse.ArgumentParser(description='Script para generar combinaciones de nombres de usuario')
+parser.add_argument('-f', '--file', metavar='FILE', required=True, help='Archivo con la lista de nombres y apellidos')
+
+# Analizar los argumentos pasados al script
+args = parser.parse_args()
+
+# Obtener el nombre del archivo desde los argumentos
+file_name = args.file
+
+# Leer el archivo de nombres y apellidos
+with open(file_name, 'r') as file:
+    names_lastnames = [line.strip().split(' ') for line in file]
+
+# Generar las combinaciones de nombres de usuario
+username_combinations = generate_username_combinations(names_lastnames)
+
+# Imprimir las combinaciones generadas
+for username in username_combinations:
+    print(username)
